907fb8ba83c037c4e18fd933a054ecabd06dfe10
sofia <sofia.maths@mail.ru>
2017-12-16 00:25:22 +0300

    nu teper tochno finito

--- Assets/Scripts/map_spots.cs
+++ Assets/Scripts/map_spots.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*using ConsoleApplication1.dll;*/
+using System.Runtime.InteropServices;
 
 public class map_spots : MonoBehaviour {
     float timer = 0;
     public Vector2[] spots;
     public GameObject tochka;
 
+    [DllImport("dielel")]               //ВОТ ТУТА
+    private static extern int get5();   //ОБЪЯВЛЯЕМ
 
     void Start () {
         spots = new Vector2[15];
 
-            for (int i = 0; i < 15; i++)
+            for (int i = 0; i < get5()/*ВОТ ТУТА ИСПОЛЬЗУЕМ*/; i++)
         {
             spots[i] = new Vector2(Random.Range(-20, 20), Random.Range(-10, 10));
             tochka.transform.position = new Vector3(spots[i][0], spots[i][1], i);
